7b57d792154977f390927a7b0b2b8f3c7a9f5252
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2021-03-22 08:10:25 -0400

    The home section is completed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Preloader
 $(window).on('load', function () { // makes sure that the whole site is loaded
     $('#status').fadeOut();
-    $('#preloader').delay(350).fadeOut();
+    $('#preloader').delay(350).fadeOut('slow');
 });
