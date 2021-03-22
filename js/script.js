48bd04a84f6c7d42acfeb0154b8fb83ddf54596c
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2021-03-22 13:03:33 -0400

    The Part 1 of the Team section is complete

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-// Preloader
+/* =================================
+|   |   |   |   |  Preloader
+====================================  */
 $(window).on('load', function () { // makes sure that the whole site is loaded
     $('#status').fadeOut();
     $('#preloader').delay(350).fadeOut('slow');
+});
+
+/* =================================
+|   |   |   |   |  Team
+====================================  */
+$(function(){
+    $("#team-members").owlCarousel({
+        items: 2,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left fa-4x"></i>', '<i class="fa fa-angle-right fa-4x"></i>']
+    });
 });
